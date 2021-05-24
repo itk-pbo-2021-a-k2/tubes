030adb40b35325772341800a642994800d26482f
Muhammad Fathurrohim <30656271+Uskrai@users.noreply.github.com>
2021-05-24 08:38:25 +0800

    Initial commit

---
+++ src/main/java/com/github/itk/pbo2021/a/k2/tubes/App.java
@@ -0,0 +1,13 @@
+package com.github.itk.pbo2021.a.k2.tubes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
