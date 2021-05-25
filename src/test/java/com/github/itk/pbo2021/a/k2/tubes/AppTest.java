5f86c1ff4a01ff26449953f8696301b4f6b28961
Muhammad Fathurrohim <30656271+Uskrai@users.noreply.github.com>
2021-05-25 09:51:38 +0800

    Create modul factory test

--- src/test/java/com/github/itk/pbo2021/a/k2/tubes/AppTest.java
+++ src/test/java/com/github/itk/pbo2021/a/k2/tubes/AppTest.java
@@ -1,20 +1,41 @@
 package com.github.itk.pbo2021.a.k2.tubes;
 
-import static org.junit.Assert.assertTrue;
-
+import com.github.itk.pbo2021.a.k2.tubes.fisdas2.ModulFactory;
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+import static org.junit.Assert.assertTrue;
+
+/** Unit test for simple App. */
+public class AppTest {
+  /** Rigorous Test :-) */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+
+  @Test
+  public void shouldNotException() {
+
+    try {
+
+      var factory = new ModulFactory();
+      FakerInput faker = new FakerInput();
+      ZeroInput zero = new ZeroInput();
+
+      for ( var formula : factory ) {
+        formula.apply(faker);
+        formula.apply(zero);
+
+        System.out.println("Faker");
+        faker.output();
+        System.out.println("Zero");
+        zero.output();
+      }
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      Assert.fail(ex.getMessage());
     }
+  }
 }
